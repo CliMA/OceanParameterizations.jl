fe90c935912fc51ed0fc11691582782d7fabe4f9
ali-ramadhan <ali.hh.ramadhan@gmail.com>
2020-03-12 09:14:55 -0400

    Training neural diffusion PDE

--- diffusion/diffusion_neural_pde.jl
+++ diffusion/diffusion_neural_pde.jl
@@ -1,3 +1,7 @@
+using DiffEqFlux
+using Flux
+using Optim
+
 include("diffusion_surrogate.jl")
 
 u₀_Gaussian(x) = exp(-50x^2)
@@ -16,15 +20,18 @@ dudt_NN = FastChain(FastDense(N, 100, tanh),
 tspan_npde = (0.0, Δt)
 diffusion_npde = NeuralODE(dudt_NN, tspan_npde, Tsit5(), reltol=1e-4, saveat=[Δt])
 
-loss_function(uₙ, uₙ₊₁) = Flux.mse(uₙ₊₁, diffusion_npde(uₙ))
+loss_function(θ, uₙ, uₙ₊₁) = Flux.mse(uₙ₊₁, diffusion_npde(uₙ, θ))
 
 opt = ADAM(1e-2)
 
-function cb()
-    train_loss = sum([loss_function(training_data[i]...) for i in 1:Nt-1])
-    println("train_loss = $train_loss")    
-    return train_loss
+function cb(θ, args...)
+    train_loss = sum([loss_function(θ, training_data[i]...) for i in 1:Nt-1])
+    println("train_loss = $train_loss")
+    return false
 end
 
-cb()
+cb(diffusion_npde.p)
+
+DiffEqFlux.sciml_train(loss_function, diffusion_npde.p, LBFGS(), training_data, cb=cb, maxiters=10)
+DiffEqFlux.sciml_train(loss_function, diffusion_npde.p, ADAM(1e-2), training_data, cb=cb, maxiters=10)
 
