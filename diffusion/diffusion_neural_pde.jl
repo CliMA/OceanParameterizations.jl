30735b605dc1c1b46f4d982447ec7c683f1a7e88
ali-ramadhan <ali.hh.ramadhan@gmail.com>
2020-03-12 22:19:22 -0400

    Multiple training stages

--- diffusion/diffusion_neural_pde.jl
+++ diffusion/diffusion_neural_pde.jl
@@ -14,8 +14,9 @@ training_data = generate_training_data(sol)
 
 animate_solution(x, sol, filename="diffusing_gaussian.mp4")
 
-dudt_NN = FastChain(FastDense(N, 100, tanh),
-                    FastDense(100, N))
+dudt_NN = FastChain(FastDense(N, N))
+#  dudt_NN = FastChain(FastDense(N, 100, tanh),
+#                      FastDense(100, N))
 
 tspan_npde = (0.0, Δt)
 diffusion_npde = NeuralODE(dudt_NN, tspan_npde, Tsit5(), reltol=1e-4, saveat=[Δt])
@@ -23,19 +24,21 @@ diffusion_npde = NeuralODE(dudt_NN, tspan_npde, Tsit5(), reltol=1e-4, saveat=[Δ
 loss_function(θ, uₙ, uₙ₊₁) = Flux.mse(uₙ₊₁, diffusion_npde(uₙ, θ))
 training_loss(θ, data) = sum([loss_function(θ, data[i]...) for i in 1:length(data)])
 
-opt = ADAM(1e-2)
-
 function cb(θ, args...)
     println("train_loss = $(training_loss(θ, training_data))")
     return false
 end
 
-cb(diffusion_npde.p)
-res1 = DiffEqFlux.sciml_train(loss_function, diffusion_npde.p, ADAM(1e-2), training_data, cb=cb, maxiters=500)
-display(res1)
-cb(res1.minimizer)
-res2 = DiffEqFlux.sciml_train(loss_function, diffusion_npde.p, LBFGS(), training_data, cb=cb, maxiters=500)
-display(res2)
-cb(res2.minimizer)
+for opt in [ADAM(1e-2), ADAM(1e-3), ADAM(1e-4), LBFGS()]
+    epochs = opt isa ADAM ? 10 : 1
+    for e in 1:epochs
+        @info "Optimizer: $(typeof(opt)) epoch $e..."
+        res = DiffEqFlux.sciml_train(loss_function, diffusion_npde.p, opt, training_data, cb=cb, maxiters=27)
+        display(res)
+        diffusion_npde.p .= res.minimizer
+        cb(diffusion_npde.p)
+        display(res.minimum)
+    end
+end
 
 test_neural_de(sol, diffusion_npde, x)
