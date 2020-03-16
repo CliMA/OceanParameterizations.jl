f711fbd55e6e6e4b9312fe1b230999f46aeb3704
ali-ramadhan <ali.hh.ramadhan@gmail.com>
2020-03-16 12:07:57 -0400

    Start merging neural PDE and GP scripts

--- diffusion/diffusion_surrogate.jl
+++ diffusion/diffusion_surrogate.jl
@@ -7,6 +7,10 @@ using Plots
 # See: https://github.com/jheinen/GR.jl/issues/278
 ENV["GKSwstype"] = "100"
 
+#####
+##### Generating solutions and training data
+#####
+
 function diffusion!(∂u∂t, u, p, t)
     N, Δx, κ = p.N, p.Δx, p.κ
     @inbounds begin
@@ -20,6 +24,8 @@ function diffusion!(∂u∂t, u, p, t)
 end
 
 """
+    solve_diffusion_equation(; u₀, N, L, κ, T, Nt)
+
 Solve a 1D diffusion equation with initial condition given by `u₀(x)` on a domain -L/2 <= x <= L/2 with diffusivity `κ` using `N` grid points for time 0 <= t <= `T`. A solution with `Nt` outputs will be returned.
 """
 function solve_diffusion_equation(; u₀, N, L, κ, T, Nt)
@@ -31,7 +37,7 @@ function solve_diffusion_equation(; u₀, N, L, κ, T, Nt)
     Δt = (tspan[2] - tspan[1]) / Nt
     t = range(tspan[1], tspan[2], length=Nt)
 
-    params = (N=N, Δx=Δx, κ=κ)
+    params = (N=N, Δx=Δx, κ=κ, x=x)
     prob = ODEProblem(diffusion!, ic, tspan, params)
     solution = solve(prob, Tsit5(), saveat=t)
 
@@ -54,18 +60,101 @@ function generate_training_data(sol)
     return training_data
 end
 
-function animate_solution(x, sol; filename)
+function generate_solutions(training_functions, testing_functions; N, L, κ, T, Nt)
+    # Generate truth solutions for training and testing
+    solutions = Dict()
+    training_data = []
+
+    for u₀ in (training_functions..., testing_functions...)
+        sol = solve_diffusion_equation(u₀=u₀, N=N, L=L, κ=κ, T=T, Nt=Nt)
+        solutions[function_name(u₀)] = sol
+
+        u₀ in training_functions && append!(training_data, generate_training_data(sol))
+
+        fname = "diffusing_$(function_name(u₀)).mp4"
+        animate_solution(x, sol, filename=fname)
+    end
+
+    return solutions, training_data
+end
+
+function animate_solution(sol; filename, fps=15)
     Nt = length(sol)
+    x = sol.prob.p.x
+
     anim = @animate for n=1:Nt
         plot(x, sol.u[n], linewidth=2, ylim=(0, 2), label="", show=false)
     end
 
     @info "Saving $filename..."
-    mp4(anim, filename, fps=15)
+    mp4(anim, filename, fps=fps)
 end
 
-function animate_neural_de_test(sol, nde, x; filename)
+#####
+##### Neural differential equation
+#####
+
+function generate_neural_pde_architecture(N)
+    # dudt_NN = FastChain(FastDense(N, N))
+
+    # dudt_NN = FastChain(FastDense(N, 100, tanh),
+    #                     FastDense(100, N))
+
+    # Conservation matrix
+    C = Matrix{Float64}(I, N, N)
+    C[end, 1:end-1] .= -1
+    C[end, end] = 0
+
+    dudt_NN = Chain(Dense(N, N), u -> C*u)
+
+    return dudt_NN
+end
+
+function train_diffusion_neural_pde(training_data, NN, optimizers)
+    # Set up neural differential equation
+    tspan_npde = (0.0, Δt)
+    diffusion_npde = NeuralODE(NN, tspan_npde, Tsit5(), reltol=1e-4, saveat=[Δt])
+
+    loss_function(θ, uₙ, uₙ₊₁) = Flux.mse(uₙ₊₁, diffusion_npde(uₙ, θ)) ./ (sum(abs2, uₙ₊₁) + 1e-6)
+    training_loss(θ, data) = sum([loss_function(θ, data[i]...) for i in 1:length(data)])
+
+    function cb(θ, args...)
+        println("train_loss = $(training_loss(θ, training_data))")
+        return false
+    end
+
+    # Train!
+    for opt in optimizers
+        @info "Training with optimizer: $(typeof(opt))..."
+        if opt isa Optim.AbstractOptimizer
+            full_loss(θ) = training_loss(θ, training_data)
+            res = DiffEqFlux.sciml_train(full_loss, diffusion_npde.p, opt, cb=cb, maxiters=100)
+            display(res)
+            diffusion_npde.p .= res.minimizer
+        else
+            epochs = 10
+            for e in 1:epochs
+                @info "Training with optimizer: $(typeof(opt)) epoch $e..."
+                res = DiffEqFlux.sciml_train(loss_function, diffusion_npde.p, opt, training_data, cb=cb)
+                diffusion_npde.p .= res.minimizer
+            end
+        end
+    end
+
+    return diffusion_npde
+end
+
+function test_diffusion_neural_pde(npde, solutions)
+    for (name, sol) in solutions
+        u_NN = animate_neural_pde_test(sol, npde, filename="NPDE_test_$name.mp4")
+        plot_conservation(u_NN, sol.t, filename="NPDE_conservation_$name.png")
+    end
+    return nothing
+end
+
+function animate_neural_pde_test(sol, nde; filename, fps=15)
     N, Nt = size(sol)
+    x = sol.prob.p.x
 
     u_NN = zeros(N, Nt)
     u_NN[:, 1] .= sol.u[1]
@@ -80,18 +169,22 @@ function animate_neural_de_test(sol, nde, x; filename)
         plot!(x, u_NN[:, n], linewidth=2, ylim=(0, 2), label="Neural PDE", show=false)
     end
 
-    mp4(anim, filename, fps=15)
+    mp4(anim, filename, fps=fps)
 
     return u_NN
 end
 
-function plot_conservation(u_NN, t; filename)
-    N, Nt = size(u_NN)
-    Σu = sum(u_NN[:, 1])
-    Σu_NN = [sum(u_NN[:, n]) for n in 1:Nt]
+#####
+##### Testing
+#####
 
-    p = plot(t, Σu_NN .- Σu, linewidth=2, title="Conservation", label="")
-    
+function plot_conservation(u, t; filename)
+    N, Nt = size(u)
+    Σu₀ = sum(u[:, 1])
+    Σu = [sum(u[:, n]) for n in 1:Nt]
+
+    p = plot(t, Σu .- Σu₀, linewidth=2, title="Conservation", label="")
+
     @info "Saving $filename..."
     savefig(p, filename)
 end
---
+++ diffusion/train_diffusion.jl
@@ -0,0 +1,50 @@
+using LinearAlgebra
+using DiffEqFlux
+using Flux
+using Optim
+using ClimateSurrogates
+
+include("diffusion_surrogate.jl")
+
+const N = 16  # Number of grid points
+const L = 2   # Domain size -L/2 <= x <= L/2
+const κ = 1   # Diffusivity
+const T = 0.1 # Time span 0 <= T <= 0.1
+const Nt = 32 # Number of time snapshots to save
+
+#####
+##### Training and testing initial condition functions
+#####
+
+u₀_Gaussian(x) = exp(-50x^2)
+u₀_quadratic(x) = 1 - x^2
+u₀_sin(x) = 1 + sin(2π * x)
+u₀_cos(x) = 1 - cos(2π * x)
+u₀_shifted_sin(x) = 1 + sin(π * x + π/3)
+u₀_shifted_cos(x) = 1 - cos(π * x - π/6)
+u₀_zero(x) = 0.0
+u₀_one(x) = 1.0
+
+function_name(::typeof(u₀_Gaussian)) = "Gaussian"
+function_name(::typeof(u₀_quadratic)) = "quadratic"
+function_name(::typeof(u₀_sin))  = "sin"
+function_name(::typeof(u₀_cos))  = "cos"
+function_name(::typeof(u₀_shifted_sin))  = "shifted_sin"
+function_name(::typeof(u₀_shifted_cos))  = "shifted_cos"
+function_name(::typeof(u₀_zero)) = "zero"
+function_name(::typeof(u₀_one)) = "one"
+
+training_functions = (u₀_Gaussian, u₀_cos, u₀_shifted_sin, u₀_zero)
+testing_functions = (u₀_quadratic, u₀_shifted_cos, u₀_sin, u₀_one)
+
+solutions, training_data =
+    generate_solutions(training_functions, testing_functions; N=N, L=L, κ=κ, T=T, Nt=Nt)
+
+#####
+##### Train and test a neural differential equation
+#####
+
+optimizers = [Descent(1e-5), LBFGS()]
+diffusion_npde = train_diffusion_neural_pde(training_data, dudt_NN, optimizers)
+
+test_diffusion_neural_pde(diffusion_npde, solutions)
