2c132d372e0857a336f6a440aa51c3c1a5d4fc91
ali-ramadhan <ali.hh.ramadhan@gmail.com>
2020-04-30 12:00:00 -0400

    Better training and inference for convection GP

--- upright_convection/upright_convection_gp.jl
+++ upright_convection/upright_convection_gp.jl
@@ -21,6 +21,7 @@ file = jldopen("free_convection_profiles.jld2")
 
 Is = keys(file["timeseries/t"])
 
+zC = file["grid/zC"]
 Nz = file["grid/Nz"]
 Lz = file["grid/Lz"]
 Nt = length(Is)
@@ -65,7 +66,7 @@ function avg(Φ, n)
 end
 
 @gen function generate_gp_kernel()
-    l ~ gamma(1, 2)
+    l ~ gamma(1, 5000)
     σ² ~ gamma(1, 2)
     kernel = SquaredExponential(l, σ²)
     return kernel
@@ -95,24 +96,49 @@ end
 function infer_gp_hyperparameters(x_train, y_train, x_test, y_test; iters)
     observations = Gen.choicemap()
 
-    for (x, y) in zip(x_test, y_test)
-        N, Nt = size(sol)
-        for n in 2:Nt, i in 1:N
-            observations[(:u, n, i)] = sol.u[n][i]
-        end
+    Nt, N = length(x_test), length(x_test[1])
+    for n in 2:Nt, i in 1:N
+        observations[(:u, n, i)] = x_test[n][i]
     end
-
-    trace, _ = Gen.generate(predict_convection_gp, (x_train, y_train, solutions), observations)
 
     gp_hyperparameters = select(:gp => :kernel => :l, :gp => :kernel => :σ²)
 
-    traces = []
+    trace, _ = Gen.generate(predict_convection_gp, (x_train, y_train, x_test, y_test), observations)
     for _ in 1:iters
-        trace, _ = metropolis_hastings(trace, gp_hyperparameters)
-        push!(traces, trace)
+        trace, accepted = metropolis_hastings(trace, gp_hyperparameters, observations=observations)
     end
 
-    return traces
+    return trace
+end
+
+function test_convection_gp(gp, x_train, y_train, x_test, y_test)
+    u = x_train[1]
+    Nt, N = length(x_train) + length(x_test), length(u)
+
+    us = []
+    push!(us, u)
+
+    for n in 2:Nt
+        u = predict(gp, [u])
+        push!(us, u)
+    end
+
+    return us
+end
+
+function animate_convection_gp(T, us)
+    Nt, N = size(T)
+
+    anim = @animate for n=1:5:Nt
+        p = plot(T[n, :], zC, linewidth=2,
+             xlim=(19, 20), ylim=(-100, 0), label="LES",
+             xlabel="Temperature (C)", ylabel="Depth (z)",
+             dpi=200, show=false)
+
+        plot!(p, us[n], zC_cs, linewidth=2, label="GP", legend=:bottomright)
+    end
+
+    mp4(anim, "deepening_mixed_layer_GP.mp4", fps=15)
 end
 
 Nt, N = size(T)
@@ -120,17 +146,33 @@ coarse_resolution = cr = 16
 Tₙ    = zeros(cr, Nt-1)
 Tₙ₊₁  = zeros(cr, Nt-1)
 
+zC_cs = avg(zC, cr)
+
 for i in 1:Nt-1
       Tₙ[:, i] .=  avg(T[i, :], cr)
     Tₙ₊₁[:, i] .=  avg(T[i+1, :], cr)
 end
 
-n_train = round(Int, (Nt-1)/2)
-training_data = [(Tₙ[:, i], Tₙ₊₁[:, i]) for i in 1:n_train]
-testing_data = [(Tₙ[:, i], Tₙ₊₁[:, i]) for i in n_train:Nt-1]
+# n_train = round(Int, (Nt-1)/2)
+# training_data = [(Tₙ[:, i], Tₙ₊₁[:, i]) for i in 1:n_train]
+# testing_data = [(Tₙ[:, i], Tₙ₊₁[:, i]) for i in n_train:Nt-1]
+
+n_train = 1:5:Nt-1
+n_test = filter(n -> n ∉ n_train, 1:Nt-1)
+
+training_data = [(Tₙ[:, n], Tₙ₊₁[:, n]) for n in n_train]
+testing_data = [(Tₙ[:, n], Tₙ₊₁[:, n]) for n in n_test]
 
 x_train = [data[1] for data in training_data]
 y_train = [data[2] for data in training_data]
 
 x_test = [data[1] for data in testing_data]
 y_test = [data[2] for data in testing_data]
+
+trace = infer_gp_hyperparameters(x_train, y_train, x_test, y_test, iters=100)
+
+l = trace[(:gp => :kernel => :l)]
+σ² = trace[(:gp => :kernel => :σ²)]
+gp = GaussianProcess(x_train, y_train, SquaredExponential(l, σ²))
+us = test_convection_gp(gp, x_train, y_train, x_test, y_test)
+animate_convection_gp(T, us)
