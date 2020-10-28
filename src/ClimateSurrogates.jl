aee93e38b01e31abc3f8c9e6d60f1f3b9db93c7e
ali-ramadhan <ali.hh.ramadhan@gmail.com>
2020-10-28 16:44:05 -0400

    Feature scaling

--- src/ClimateSurrogates.jl
+++ src/ClimateSurrogates.jl
@@ -1,12 +1,12 @@
-module ClimateSurrogates
-
-# using DiffEqFlux
+module ClimateSurrogatesTalk abou
 
 export
     coarse_grain, Dᶠ, Dᶜ,
+    UnitMeanZeroVarianceScaling, MinMaxScaling, scale, unscale,
     GaussianProcess, predict, uncertainty, SquaredExponential
 
 include("coarse_graining.jl")
+include("feature_scaling.jl")
 include("differentiation_operators.jl")
 include("gaussian_process.jl")
 
---
+++ src/feature_scaling.jl
@@ -0,0 +1,39 @@
+abstract type AbstractFeatureScaling end
+
+struct UnitMeanZeroVarianceScaling{T} <: AbstractFeatureScaling
+    μ :: T
+    σ :: T
+end
+
+"""
+    UnitMeanZeroVarianceScaling(data)
+
+Returns a feature scaler for `data` with zero mean and unit variance.
+"""
+function UnitMeanZeroVarianceScaling(data)
+    μ, σ = mean(data), std(data)
+    return UnitMeanZeroVarianceScaling(μ, σ)
+end
+
+scale(x, s::UnitMeanZeroVarianceScaling) = (x - s.μ) / s.σ
+unscale(y, s::UnitMeanZeroVarianceScaling) = s.σ * y + s.μ
+
+struct MinMaxScaling{T} <: AbstractFeatureScaling
+           a :: T
+           b :: T
+    data_min :: T
+    data_max :: T
+end
+
+"""
+    MinMaxScaling(data; a=0, b=1)
+
+Returns a feature scaler for `data` with minimum `a` and `maximum `b`.
+"""
+function MinMaxScaling(data; a=0, b=1)
+    data_min, data_max = extrema(data)
+    return MinMaxScaling{typeof(data_min)}(a, b, data_min, data_max)
+end
+
+scale(x, s::MinMaxScaling) = s.a + (x - s.data_min) * (s.b - s.a) / (s.data_max - s.data_min)
+unscale(y, s::MinMaxScaling) = s.data_min + (y - s.a) * (s.data_max - s.data_min) / (s.b - s.a)
