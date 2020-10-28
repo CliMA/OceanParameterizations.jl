8145073cf6c3e297dbd7af91e5947cd1f7e8a52d
ali-ramadhan <ali.hh.ramadhan@gmail.com>
2020-10-28 06:37:25 -0400

    Heat flux figure

--- free_convection/free_convection_oceananigans.jl
+++ free_convection/free_convection_oceananigans.jl
@@ -56,7 +56,7 @@ end
 
 ρ₀ = 1027
 cₚ = 4000
-Q  = 75
+Q  = 100
 ∂T₀∂z = 0.01
 K = 1000
 
@@ -99,8 +99,8 @@ NN = FastChain(standardize_T,
                FastDense( Nz, 4Nz, relu),
                FastDense(4Nz, 4Nz, relu),
                FastDense(4Nz, Nz-1),
-               enforce_fluxes,
                standardize⁻¹_wT,
+               enforce_fluxes,
                ∂z_wT)
 
 ∂z_wT_NN = zeros(Nz)
@@ -124,6 +124,9 @@ stop_time = 6day
 Nt = Int(stop_time / Δt)
 
 ds = ds = NCDataset("free_convection_horizontal_averages_$(Q)W.nc")
+
+T_NN_fig = zeros(Nz, Nt+1)
+T_NN_fig[:, 1] .= interior(model_neural_network.tracers.T)[:]
 
 anim = @animate for n in 1:Nt
     T = interior(model_neural_network.tracers.T)[:]
@@ -149,8 +152,82 @@ anim = @animate for n in 1:Nt
     # plot!(T_CA, z, linewidth=2, label="Oceananigans 1D + convective adjustment", show=false)
 
     plot!(T_NN, z, linewidth=2, label="Oceananigans 1D + convective adjustment + neural PDE", show=false)
+
+    T_NN_fig[:, n+1] .= T_NN
 end
 
 mp4(anim, "oceananigans_free_convection_$(Q)W.mp4", fps=60)
 
+import PyPlot
+const plt = PyPlot
+const Line2D = plt.matplotlib.lines.Line2D
+const Patch = plt.matplotlib.patches.Patch
+
+fig, ax = plt.subplots(nrows=1, ncols=1, figsize=(6, 4), dpi=200)
+
+time_indices = (1, 109, 433, 865)
+colors = ("tab:blue", "tab:orange", "tab:green", "tab:red")
+
+for (n, color) in zip(time_indices, colors)
+    ax.plot(ds["T"][:, n], ds["zC"][:], color=color, alpha=0.4)
+    ax.plot(T_NN_fig[:, n], znodes(Cell, grid), color=color, linestyle="--")
+end
+
+ax.set_xlabel("Temperature (°C)")
+ax.set_ylabel("z (m)")
+ax.set_xlim([19, 20])
+ax.set_ylim([-100, 0])
+
+ax.text(19.98, 1, "0 hours",  color="tab:blue",   rotation=45)
+ax.text(19.79, 1, "18 hours", color="tab:orange", rotation=45)
+ax.text(19.60, 1, "3 days",   color="tab:green",  rotation=45)
+ax.text(19.46, 1, "6 days",   color="tab:red",    rotation=45)
+
+custom_lines = [
+    Line2D([0], [0], color="black", linestyle="-", alpha=0.5),
+    Line2D([0], [0], color="black", linestyle="--")
+]
+
+ax.legend(custom_lines, ["Oceananigans 3D", "Oceananigans 1D + neural PDE"], loc="lower right", frameon=false)
+
+plt.savefig("neurips_npde_figure.png")
+
 close(ds)
+
+grid_points = 32
+T, wT, z = ds["T"], ds["wT"], ds["zC"]
+zF_coarse = coarse_grain(ds["zF"], grid_points+1, Face)
+
+S_T, S⁻¹_wT = standardization.T.standardize, standardization.wT.standardize⁻¹
+
+fig, ax = plt.subplots(nrows=1, ncols=1, figsize=(6, 4), dpi=200)
+
+time_indices = (109, 433, 865) |> reverse
+colors = ("tab:orange", "tab:green", "tab:red") |> reverse
+
+for (n, color) in zip(time_indices, colors)
+    wT = ds["wT"][:, n]
+    wT[end] = Q / ρ₀ / cₚ
+    ax.plot(wT / 1e-5, ds["zF"][:], color=color, alpha=0.5)
+
+    wT_NN = coarse_grain(T[:, n], grid_points, Cell) .|> S_T |> NN .|> S⁻¹_wT
+    wT_NN[end-1] = (wT_NN[end-2] + wT_NN[end]) / 2
+    ax.plot(wT_NN / 1e-5, zF_coarse, color=color, linestyle="--")
+end
+
+ax.set_xlabel("Turbulent vertical heat flux (×10⁻⁵ m/s K)")
+ax.set_ylabel("z (m)")
+ax.set_xlim([-0.5, 2.5])
+ax.set_ylim([-100, 0])
+
+custom_lines = [
+    Patch(facecolor="tab:orange"),
+    Patch(facecolor="tab:green"),
+    Patch(facecolor="tab:red"),
+    Line2D([0], [0], color="black", linestyle="-", alpha=0.5),
+    Line2D([0], [0], color="black", linestyle="--")
+]
+
+ax.legend(custom_lines, ["18 hours", "3 days", "6 days", "Oceananigans 3D", "Neural network"], loc="lower right", frameon=false)
+
+plt.savefig("neurips_heat_flux_figure.png")
