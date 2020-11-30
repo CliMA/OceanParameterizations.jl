627599758ae35bb96c54b0a8b200922cf8fa912b
Adeline Hillier <51460867+adelinehillier@users.noreply.github.com>
2020-11-30 11:25:45 -0800

    Revert "Modularize and add GPR + KPP + TKE"
    This reverts commit fbaed107a0152bbb9b911fd2629261ac4accf3e3.

--- src/ClimateParameterizations.jl
+++ src/ClimateParameterizations.jl
@@ -34,94 +34,15 @@ using DiffEqSensitivity: InterpolatingAdjoint, ZygoteVJP
 
 import Base.inv
 
+include("coarse_graining.jl")
+include("feature_scaling.jl")
 include("differentiation_operators.jl")
-# include("gaussian_process.jl")
+include("gaussian_process.jl")
 
 include("ocean_convection.jl")
 
 function __init__()
     Logging.global_logger(OceananigansLogger())
 end
-
-export
-    # Data / profile_data.jl
-    data,
-    VData,
-    ProfileData,
-
-    # Data / modify_predictor_fns.jl,
-    # append_tke,
-    # partial_temp_profile,
-
-    # NeuralNetwork / NeuralNetwork.jl
-    nn_model,
-
-    # GaussianProcess / gp.jl
-    gp_model,
-
-    # GaussianProcess / kernels.jl
-    Kernel,
-    get_kernel,
-    kernel_function,
-
-    # GaussianProcess / distances.jl
-    euclidean_distance,
-    derivative_distance,
-    antiderivative_distance,
-
-    # GaussianProcess / hyperparameter_landscapes.jl
-    plot_landscapes_compare_error_metrics,
-    plot_landscapes_compare_files_me,
-    plot_error_histogram,
-    get_min_gamma,
-    get_min_gamma_alpha,
-    train_validate_test,
-
-    # Main
-    mean_square_error,
-    predict,
-
-    # Main / plot_profile.jl
-    plot_profile,
-    plot_model_output,
-    animate_profile,
-    animate_profile_and_model_output,
-
-    animate_gif
-
-    # kernel options
-    #  1   =>   "Squared exponential"         => "Squared exponential kernel:        k(x,x') = σ * exp( - ||x-x'||² / 2γ² )",
-    #  2   =>   "Matern 1/2"                  => "Matérn with ʋ=1/2:                 k(x,x') = σ * exp( - ||x-x'|| / γ )",
-    #  3   =>   "Matern 3/2"                  => "Matérn with ʋ=3/2:                 k(x,x') = σ * (1+c) * exp(-√(3)*||x-x'||)/γ)",
-    #  4   =>   "Matern 5/2"                  => "Matérn with ʋ=5/2:                 k(x,x') = σ * ( 1 + √(5)*||x-x'||)/γ + 5*||x-x'||²/(3*γ^2) ) * exp(-√(5)*||x-x'||)/γ)",
-    #  5   =>   "Rational quadratic w/ α=1"   => "Rational quadratic kernel:         k(x,x') = σ * (1+(x-x')'*(x-x')/(2*α*(γ²))^(-α)",
-
-# modules
-using Plots,
-      JLD2,
-      NetCDF,
-      Statistics,
-      # LinearAlgebra,
-      BenchmarkTools,
-      Optim,
-      Statistics,
-      Flux,
-      DiffEqFlux,
-      Oceananigans.Grids
-
-# OceanTurb for KPP
-using OceanTurb
-export KPP, TKEMassFlux
-
-# submodules
-include("data/Data.jl")
-include("gpr/GaussianProcess.jl")
-include("nn/NeuralNetwork.jl")
-include("main/Main.jl")
-
-using .Data
-using .GaussianProcess
-using .NeuralNetwork
-using .Main
 
 end # module
