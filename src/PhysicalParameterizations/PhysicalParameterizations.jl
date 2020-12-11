29886c2d1d737f1e57382134afa291baa1790f0f
Adeline Hillier <adelineh2000@gmail.com>
2020-12-10 18:03:06 -0800

    Change name for KPP and TKE functions

--- src/PhysicalParameterizations/PhysicalParameterizations.jl
+++ src/PhysicalParameterizations/PhysicalParameterizations.jl
@@ -2,6 +2,10 @@ module PhysicalParameterizations
 
 using OceanTurb
 using OceanParameterizations.DataWrangling
+using Oceananigans.Grids: Cell, Face
+
+export closure_kpp_full_evolution,
+       closure_tke_full_evolution
 
 include("k_profile_parameterization.jl")
 include("turbulent_kinetic_energy_closure.jl")
