388a317fa7691d15e8e06a07894ad29ec062d536
Adeline Hillier <adelineh2000@gmail.com>
2020-12-11 23:47:11 -0800

    Add mse function to WindMixing.jl

--- wind_mixing/src/WindMixing.jl
+++ wind_mixing/src/WindMixing.jl
@@ -1,11 +1,14 @@
 module WindMixing
 
 export data, read_les_output,
-       animate_prediction
+       animate_prediction,
+       mse
 
 using Flux, Plots
 using Oceananigans.Grids: Cell, Face
 using OceanParameterizations
+
+mse(x::Tuple{Array{Float64,2}, Array{Float64,2}}) = Flux.mse(x[1], x[2])
 
 include("lesbrary_data.jl")
 include("data_containers.jl")
