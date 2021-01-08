dfe585fc75244c4aa38d6b2928fc704a653e4fa4
Xky <xin.lee18@imperial.ac.uk>
2021-01-08 03:57:34 +0000

    number of simulations error

--- wind_mixing/train_NDE.jl
+++ wind_mixing/train_NDE.jl
@@ -18,14 +18,14 @@ wT_NN = BSON.load(joinpath(PATH, "NDEs", "wT_NN_large.bson"))[:neural_network]
 
 train_epochs = [1, 1, 1]
 train_tranges = [1:5:50, 1:5:100, 1:10:200]
-train_optimizers = [[ADAM()] for i in 1:3]
+train_optimizers = [[ADAM(0.01)] for i in 1:3]
 timestepper = ROCK4()
 
 function train(FILE_PATH, train_files, train_epochs, train_tranges, train_optimizers, uw_NN, vw_NN, wT_NN, 𝒟train, timestepper)
     write_metadata_NDE_training(FILE_PATH, train_files, train_epochs, train_tranges, train_optimizers, uw_NN, vw_NN, wT_NN)
 
     for i in 1:length(train_epochs)
-        uw_NN, vw_NN, wT_NN = train_NDE_convective_adjustment(uw_NN, vw_NN, wT_NN, 𝒟train, train_tranges[i], timestepper, train_optimizers[i], train_epochs[i], FILE_PATH, i, 2)
+        uw_NN, vw_NN, wT_NN = train_NDE_convective_adjustment(uw_NN, vw_NN, wT_NN, 𝒟train, train_tranges[i], timestepper, train_optimizers[i], train_epochs[i], FILE_PATH, i, 1)
     end
 
 end
