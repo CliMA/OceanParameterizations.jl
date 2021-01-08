fb17732f931b192c5c10cb3bfd31016909f04b35
Xky <xin.lee18@imperial.ac.uk>
2021-01-08 03:51:40 +0000

    changed variable scoping

--- wind_mixing/train_NDE.jl
+++ wind_mixing/train_NDE.jl
@@ -12,8 +12,8 @@ PATH=pwd()
 FILE_PATH = joinpath(PATH, "training_output", "NDE_training_convective_adjustment_1sim_-1e-3_large.jld2")
 
 uw_NN = BSON.load(joinpath(PATH, "NDEs", "uw_NN_large.bson"))[:neural_network]
-vw_NN = BSON.load(joinpath(PATH, "NDEs", "uw_NN_large.bson"))[:neural_network]
-wT_NN = BSON.load(joinpath(PATH, "NDEs", "uw_NN_large.bson"))[:neural_network]
+vw_NN = BSON.load(joinpath(PATH, "NDEs", "vw_NN_large.bson"))[:neural_network]
+wT_NN = BSON.load(joinpath(PATH, "NDEs", "wT_NN_large.bson"))[:neural_network]
 
 
 train_epochs = [1, 1, 1]
@@ -21,8 +21,13 @@ train_tranges = [1:5:50, 1:5:100, 1:10:200]
 train_optimizers = [[ADAM()] for i in 1:3]
 timestepper = ROCK4()
 
-write_metadata_NDE_training(FILE_PATH, train_files, train_epochs, train_tranges, train_optimizers, uw_NN, vw_NN, wT_NN)
+function train(FILE_PATH, train_files, train_epochs, train_tranges, train_optimizers, uw_NN, vw_NN, wT_NN, 𝒟train, timestepper)
+    write_metadata_NDE_training(FILE_PATH, train_files, train_epochs, train_tranges, train_optimizers, uw_NN, vw_NN, wT_NN)
 
-for i in 1:length(train_epochs)
-    uw_NN, vw_NN, wT_NN = train_NDE_convective_adjustment(uw_NN, vw_NN, wT_NN, 𝒟train, train_tranges[i], timestepper, train_optimizers[i], train_epochs[i], FILE_PATH, i, 2)
+    for i in 1:length(train_epochs)
+        uw_NN, vw_NN, wT_NN = train_NDE_convective_adjustment(uw_NN, vw_NN, wT_NN, 𝒟train, train_tranges[i], timestepper, train_optimizers[i], train_epochs[i], FILE_PATH, i, 2)
+    end
+
 end
+
+train(FILE_PATH, train_files, train_epochs, train_tranges, train_optimizers, uw_NN, vw_NN, wT_NN, 𝒟train, timestepper)
