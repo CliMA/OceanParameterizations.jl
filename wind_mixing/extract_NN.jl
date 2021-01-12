19f90a121fc53721f3b793a0238143ea45748ce1
Xky <xin.lee18@imperial.ac.uk>
2021-01-12 18:34:04 +0000

    extract partial data

--- wind_mixing/extract_NN.jl
+++ wind_mixing/extract_NN.jl
@@ -4,7 +4,7 @@ using Flux
 using OrdinaryDiffEq, DiffEqSensitivity
 
 FILE_PATH = joinpath(pwd(), "training_output", "NDE_training_1sim_convective_adjustment.jld2")
-OUTPUT_PATH = joinpath(pwd(), "training_output", "NDE_training_1sim_convective_adjustment_temp.jld2")
+OUTPUT_PATH = joinpath(pwd(), "training_output", "NDE_training_1sim_convective_adjustment_temp2.jld2")
 
 @info "Opening file"
 file = jldopen(FILE_PATH, "r")
@@ -12,11 +12,12 @@ file = jldopen(FILE_PATH, "r")
 N_stages = length(keys(file["training_data/loss"]))
 
 @info "Loading file"
-training_info = file["training_info"]
+# training_info = file["training_info"]
 uw_NNs = file["training_data/neural_network/uw/$(N_stages-1)"]
 vw_NNs = file["training_data/neural_network/vw/$(N_stages-1)"]
 wT_NNs = file["training_data/neural_network/wT/$(N_stages-1)"]
 
+N_data = length(keys(uw_NNs))
 losses = file["training_data/loss/$(N_stages-1)"]
 
 @info "Writing file"
@@ -27,16 +28,16 @@ jldopen(OUTPUT_PATH, "w") do file
     # end
 
     @info "Writing NNs"
-    for key in keys(uw_NNs)
-        file["neural_network/uw/$key"] = uw_NNs[key]
-        file["neural_network/vw/$key"] = vw_NNs[key]
-        file["neural_network/wT/$key"] = wT_NNs[key]
+    for i in N_data-100:1:N_data
+        file["neural_network/uw/$i"] = uw_NNs["$i"]
+        file["neural_network/vw/$i"] = vw_NNs["$i"]
+        file["neural_network/wT/$i"] = wT_NNs["$i"]
     end
 
     @info "Writing losses"
 
-    for key in keys(losses)
-        file["loss/$key"] = losses[key]
+    for i in N_data-100:1:N_data
+        file["loss/$i"] = losses["$i"]
     end
 
 end
--- wind_mixing/src/WindMixing.jl
+++ wind_mixing/src/WindMixing.jl
@@ -5,7 +5,7 @@ export data, read_les_output,
        mse, 
        predict_NDE, predict_NDE_convective_adjustment,
        train_NDE, train_NDE_convective_adjustment, train_NN, 
-       NDE_profile, NDE_profile_convective_adjustment, animate_NN, animate_profile, animate_flux, animate_profile_flux 
+       NDE_profile, NDE_profile_convective_adjustment, animate_NN, animate_profile, animate_flux, animate_profile_flux, 
        write_metadata_NDE_training, write_data_NDE_training,
        write_metadata_NN_training, write_data_NN_training
 
