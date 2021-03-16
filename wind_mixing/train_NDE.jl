609c35869337bc66a273d7e957f215e5b7b271f5
Xky <xin.lee18@imperial.ac.uk>
2021-03-16 04:42:59 +0000

    new training for 2 sims, 0.5 diffusivity, 0.01 step

--- wind_mixing/train_NDE.jl
+++ wind_mixing/train_NDE.jl
@@ -8,7 +8,7 @@ using Random
 using GalacticOptim
 
 # Training data
-train_files = ["-1e-3"]
+train_files = ["-1e-3", "-8e-4"]
 
 𝒟train = WindMixing.data(train_files, scale_type=ZeroMeanUnitVarianceScaling, enforce_surface_fluxes=true)
 # 
@@ -17,12 +17,12 @@ PATH = pwd()
 OUTPUT_PATH = joinpath(PATH, "training_output")
 # OUTPUT_PATH = "D:\\University Matters\\Massachusetts Institute of Technology\\CLiMA Project\\OceanParameterizations.jl\\training_output"
 
-FILE_PATH = joinpath(OUTPUT_PATH, "NDE_training_modified_pacanowski_philander_1sim_-1e-3_diffusivity_5e-1_Ri_1e-2_new.jld2")
+FILE_PATH = joinpath(OUTPUT_PATH, "NDE_training_modified_pacanowski_philander_2sim_-1e-3_-8e-4_diffusivity_5e-1_Ri_1e-2_new.jld2")
 @assert !isfile(FILE_PATH)
 
-FILE_PATH_uw = joinpath(PATH, "extracted_training_output", "uw_NN_training_1sim_-1e-3_extracted.jld2")
-FILE_PATH_vw = joinpath(PATH, "extracted_training_output", "vw_NN_training_1sim_-1e-3_extracted.jld2")
-FILE_PATH_wT = joinpath(PATH, "extracted_training_output", "wT_NN_training_1sim_-1e-3_extracted.jld2")
+FILE_PATH_uw = joinpath(PATH, "extracted_training_output", "uw_NN_training_2sim_-1e-3_-8e-4_large_extracted.jld2")
+FILE_PATH_vw = joinpath(PATH, "extracted_training_output", "vw_NN_training_2sim_-1e-3_-8e-4_large_extracted.jld2")
+FILE_PATH_wT = joinpath(PATH, "extracted_training_output", "wT_NN_training_2sim_-1e-3_-8e-4_large_extracted.jld2")
 
 uw_file = jldopen(FILE_PATH_uw, "r")
 vw_file = jldopen(FILE_PATH_vw, "r")
