ad7ce2233f98f4384188337e93a5aa790716bacb
Xky <xin.lee18@imperial.ac.uk>
2021-03-20 05:34:00 +0000

    new training on 2 sim diffusivity 0.5, step 0.01

--- wind_mixing/train_NDE.jl
+++ wind_mixing/train_NDE.jl
@@ -8,7 +8,7 @@ using Random
 using GalacticOptim
 
 # Training data
-train_files = ["-1e-3"]
+train_files = ["-1e-3", "8e-4"]
 
 𝒟train = WindMixing.data(train_files, scale_type=ZeroMeanUnitVarianceScaling, enforce_surface_fluxes=true)
 # 
@@ -17,36 +17,36 @@ PATH = pwd()
 OUTPUT_PATH = joinpath(PATH, "training_output")
 # OUTPUT_PATH = "D:\\University Matters\\Massachusetts Institute of Technology\\CLiMA Project\\OceanParameterizations.jl\\training_output"
 
-FILE_PATH = joinpath(OUTPUT_PATH, "NDE_training_modified_pacanowski_philander_1sim_-1e-3_diffusivity_5e-1_Ri_1e-2_new_2.jld2")
+FILE_PATH = joinpath(OUTPUT_PATH, "NDE_training_modified_pacanowski_philander_2sim_-1e-3_-8e-4_diffusivity_5e-1_Ri_1e-2_new.jld2")
 @assert !isfile(FILE_PATH)
 
-# FILE_PATH_uw = joinpath(PATH, "extracted_training_output", "uw_NN_training_2sim_-1e-3_-8e-4_large_extracted.jld2")
-# FILE_PATH_vw = joinpath(PATH, "extracted_training_output", "vw_NN_training_2sim_-1e-3_-8e-4_large_extracted.jld2")
-# FILE_PATH_wT = joinpath(PATH, "extracted_training_output", "wT_NN_training_2sim_-1e-3_-8e-4_large_extracted.jld2")
+FILE_PATH_uw = joinpath(PATH, "extracted_training_output", "uw_NN_training_2sim_-1e-3_-8e-4_large_extracted.jld2")
+FILE_PATH_vw = joinpath(PATH, "extracted_training_output", "vw_NN_training_2sim_-1e-3_-8e-4_large_extracted.jld2")
+FILE_PATH_wT = joinpath(PATH, "extracted_training_output", "wT_NN_training_2sim_-1e-3_-8e-4_large_extracted.jld2")
 
-# uw_file = jldopen(FILE_PATH_uw, "r")
-# vw_file = jldopen(FILE_PATH_vw, "r")
-# wT_file = jldopen(FILE_PATH_wT, "r")
+uw_file = jldopen(FILE_PATH_uw, "r")
+vw_file = jldopen(FILE_PATH_vw, "r")
+wT_file = jldopen(FILE_PATH_wT, "r")
 
-# uw_NN = uw_file["neural_network"]
-# vw_NN = vw_file["neural_network"]
-# wT_NN = wT_file["neural_network"]
+uw_NN = uw_file["neural_network"]
+vw_NN = vw_file["neural_network"]
+wT_NN = wT_file["neural_network"]
 
-FILE_PATH_NN = joinpath(PATH, "extracted_training_output", "NDE_training_modified_pacanowski_philander_1sim_-1e-3_diffusivity_5e-1_Ri_1e-2_new_extracted.jld2")
+# FILE_PATH_NN = joinpath(PATH, "extracted_training_output", "NDE_training_modified_pacanowski_philander_1sim_-1e-3_diffusivity_5e-1_Ri_1e-2_new_extracted.jld2")
 
-@assert isfile(FILE_PATH_NN)
-file = jldopen(FILE_PATH_NN, "r")
+# @assert isfile(FILE_PATH_NN)
+# file = jldopen(FILE_PATH_NN, "r")
 
-uw_NN = file["neural_network/uw"]
-vw_NN = file["neural_network/vw"]
-wT_NN = file["neural_network/wT"]
+# uw_NN = file["neural_network/uw"]
+# vw_NN = file["neural_network/vw"]
+# wT_NN = file["neural_network/wT"]
 
 train_parameters = Dict("ν₀" => 1f-4, "ν₋" => 0.5f0, "Riᶜ" => 0.25f0, "ΔRi" => 1f-2, "Pr" => 1f0, "modified_pacanowski_philander" => true, "convective_adjustment" => false)
 
-train_epochs = [1]
-train_tranges = [1:30:1153]
-train_iterations = [300]
-train_optimizers = [[ADAM(1e-3), ADAM(5e-4), ADAM(2e-4)]]
+# train_epochs = [1]
+# train_tranges = [1:30:1153]
+# train_iterations = [300]
+# train_optimizers = [[ADAM(1e-3), ADAM(5e-4), ADAM(2e-4)]]
 
 # train_epochs = [1]
 # train_tranges = [1:20:100]
@@ -58,10 +58,10 @@ train_optimizers = [[ADAM(1e-3), ADAM(5e-4), ADAM(2e-4)]]
 # train_iterations = [50, 50, 100, 30, 20, 50, 150]
 # train_optimizers = [[ADAM(0.1), ADAM(0.01)], [ADAM(0.01)], [ADAM(0.01)], [ADAM(0.01)], [ADAM(0.01)], [ADAM(0.01)], [ADAM(0.01), ADAM(0.001), ADAM(5e-4), ADAM(2e-4)]]
 
-# train_tranges = [1:10:100, 1:10:200, 1:20:500, 1:20:700, 1:20:800, 1:20:900, 1:20:1153]
-# train_epochs = [1 for i in 1:length(train_tranges)]
-# train_iterations = [50, 50, 50, 30, 40, 50, 100]
-# train_optimizers = [[[ADAM(0.01)] for i in 1:6]; [[ADAM(1e-3), ADAM(1e-3), ADAM(1e-3), ADAM(5e-4), ADAM(2e-4)]]]
+train_tranges = [1:10:100, 1:10:200, 1:20:500, 1:20:700, 1:20:800, 1:20:900, 1:20:1153]
+train_epochs = [1 for i in 1:length(train_tranges)]
+train_iterations = [50, 50, 50, 30, 40, 50, 100]
+train_optimizers = [[[ADAM(0.01)] for i in 1:6]; [[ADAM(1e-3), ADAM(1e-3), ADAM(1e-3), ADAM(5e-4), ADAM(2e-4)]]]
 
 timestepper = ROCK4()
 
