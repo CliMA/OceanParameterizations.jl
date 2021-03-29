59d5d91188652f58e88ec06e6b79173c724be38b
Xky <xin.lee18@imperial.ac.uk>
2021-03-29 03:35:30 +0100

    training with modified pacanowski philander, smoothing NN

--- wind_mixing/train_NDE.jl
+++ wind_mixing/train_NDE.jl
@@ -17,7 +17,7 @@ PATH = pwd()
 OUTPUT_PATH = joinpath(PATH, "training_output")
 # OUTPUT_PATH = "D:\\University Matters\\Massachusetts Institute of Technology\\CLiMA Project\\OceanParameterizations.jl\\training_output"
 
-FILE_PATH = joinpath(OUTPUT_PATH, "NDE_training_1sim_-1e-3_smooth_NN_3.jld2")
+FILE_PATH = joinpath(OUTPUT_PATH, "NDE_training_modified_pacalowski_philander_1sim_-1e-3_smooth_NN.jld2")
 @assert !isfile(FILE_PATH)
 
 FILE_PATH_uw = joinpath(PATH, "extracted_training_output", "uw_NN_training_1sim_-1e-3_extracted.jld2")
@@ -42,7 +42,7 @@ wT_NN = wT_file["neural_network"]
 # wT_NN = file["neural_network/wT"]
 
 train_parameters = Dict("ν₀" => 1f-4, "ν₋" => 0.1f0, "Riᶜ" => 0.25f0, "ΔRi" => 1f-1, "Pr" => 1f0, 
-                        "modified_pacanowski_philander" => false, "convective_adjustment" => false,
+                        "modified_pacanowski_philander" => true, "convective_adjustment" => false,
                         "smooth_profile" => false, "smooth_NN" => true)
 
 # train_epochs = [1]
