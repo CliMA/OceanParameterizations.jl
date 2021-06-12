8c78b5d6f5c19ed451352505c6765949008a8e85
Xky <xin.lee18@imperial.ac.uk>
2021-06-12 04:35:22 +0100

    optimize mpp on 10sim

--- wind_mixing/optimise_modified_pacanowski_philander.jl
+++ wind_mixing/optimise_modified_pacanowski_philander.jl
@@ -6,17 +6,22 @@ using Flux
 using GalacticOptim
 
 train_files = [       
+    "wind_-1e-3_heating_-4e-8",
+    "wind_-1e-3_heating_-1e-8",
+    "wind_-5e-4_heating_-3e-8",
+    "wind_-2e-4_heating_-5e-8",
+    "wind_-2e-4_heating_-1e-8",
     "wind_-1e-3_cooling_4e-8",
+    "wind_-2e-4_cooling_1e-8",
     "wind_-1e-3_cooling_2e-8",
     "wind_-2e-4_cooling_5e-8",
-    "wind_-1e-3_heating_-4e-8",
-    "wind_-2e-4_heating_-5e-8",
+    "wind_-5e-4_cooling_3e-8",
 ]
 
 PATH = pwd()
 # PATH = "D:\\University Matters\\MIT\\CLiMA Project\\OceanParameterizations.jl"
 
-FILE_NAME = "parameter_optimisation_5sim_windcooling_SS_SW_WS_windheating_SS_WS_5_params_BFGS"
+FILE_NAME = "parameter_optimisation_10sim_windcooling_windheating_5params_BFGS"
 OUTPUT_PATH = joinpath(PATH, "training_output", "$(FILE_NAME).jld2")
 
 EXTRACTED_OUTPUT_PATH = joinpath(PATH, "extracted_training_output", "$(FILE_NAME)_extracted.jld2")
