0e4beec26be4691af456316740de44893d530c13
Xky <xin.lee18@imperial.ac.uk>
2021-06-12 04:39:24 +0100

    optimise mpp on 18sim

--- wind_mixing/optimise_modified_pacanowski_philander.jl
+++ wind_mixing/optimise_modified_pacanowski_philander.jl
@@ -6,22 +6,30 @@ using Flux
 using GalacticOptim
 
 train_files = [       
+    "wind_-5e-4_cooling_4e-8", 
+    "wind_-1e-3_cooling_4e-8", 
+    "wind_-2e-4_cooling_1e-8", 
+    "wind_-1e-3_cooling_2e-8", 
+    "wind_-5e-4_cooling_1e-8", 
+    "wind_-2e-4_cooling_5e-8", 
+    "wind_-5e-4_cooling_3e-8", 
+    "wind_-2e-4_cooling_3e-8", 
+    "wind_-1e-3_cooling_3e-8", 
     "wind_-1e-3_heating_-4e-8",
     "wind_-1e-3_heating_-1e-8",
+    "wind_-1e-3_heating_-3e-8",
+    "wind_-5e-4_heating_-5e-8",
     "wind_-5e-4_heating_-3e-8",
+    "wind_-5e-4_heating_-1e-8",
     "wind_-2e-4_heating_-5e-8",
+    "wind_-2e-4_heating_-3e-8",
     "wind_-2e-4_heating_-1e-8",
-    "wind_-1e-3_cooling_4e-8",
-    "wind_-2e-4_cooling_1e-8",
-    "wind_-1e-3_cooling_2e-8",
-    "wind_-2e-4_cooling_5e-8",
-    "wind_-5e-4_cooling_3e-8",
 ]
 
 PATH = pwd()
 # PATH = "D:\\University Matters\\MIT\\CLiMA Project\\OceanParameterizations.jl"
 
-FILE_NAME = "parameter_optimisation_10sim_windcooling_windheating_5params_BFGS"
+FILE_NAME = "parameter_optimisation_18sim_windcooling_windheating_5params_BFGS"
 OUTPUT_PATH = joinpath(PATH, "training_output", "$(FILE_NAME).jld2")
 
 EXTRACTED_OUTPUT_PATH = joinpath(PATH, "extracted_training_output", "$(FILE_NAME)_extracted.jld2")
