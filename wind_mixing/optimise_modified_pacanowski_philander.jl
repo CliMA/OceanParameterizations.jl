b0b8743ad10d19aef8ce240926982bcc406d9dee
Xky <xin.lee18@imperial.ac.uk>
2021-06-13 05:45:20 +0100

    mpp optimisation with gradient descent

--- wind_mixing/optimise_modified_pacanowski_philander.jl
+++ wind_mixing/optimise_modified_pacanowski_philander.jl
@@ -29,14 +29,14 @@ train_files = [
 PATH = pwd()
 # PATH = "D:\\University Matters\\MIT\\CLiMA Project\\OceanParameterizations.jl"
 
-FILE_NAME = "parameter_optimisation_18sim_windcooling_windheating_5params_CG"
+FILE_NAME = "parameter_optimisation_18sim_windcooling_windheating_5params_GD"
 OUTPUT_PATH = joinpath(PATH, "training_output", "$(FILE_NAME).jld2")
 
 EXTRACTED_OUTPUT_PATH = joinpath(PATH, "extracted_training_output", "$(FILE_NAME)_extracted.jld2")
 
 timestepper = ROCK4()
 
-optimizers = [ConjugateGradient()]
+optimizers = [Descent()]
 
 tsteps = 1:20:1153
 maxiters = 200
