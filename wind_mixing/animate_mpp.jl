ba843c066612b8d9131ba49c387d72d603eeec40
Xky <xin.lee18@imperial.ac.uk>
2021-06-13 04:59:38 +0100

    animate mpp with optimised parameters

--- wind_mixing/animate_mpp.jl
+++ wind_mixing/animate_mpp.jl
@@ -15,7 +15,14 @@ PATH = joinpath(pwd(), "extracted_training_output")
 DATA_NAME = "NDE_training_mpp_8sim_wind_mixing_cooling_diffusivity_1e-1_Ri_1e-1_weights_divide1f5_gradient_smallNN_scale_5e-3_rate_1e-4"
 DATA_PATH = joinpath(PATH, "$(DATA_NAME)_extracted.jld2")
 ispath(DATA_PATH)
-FILE_PATH = joinpath(pwd(), "Output", "mpp")
+
+FILE_PATH = joinpath(pwd(), "Output", "mpp_18sim_5params_BFGS")
+
+if !isfile(FILE_PATH)
+    mkdir(FILE_PATH)
+end
+
+PARAMETERS_PATH = joinpath(PATH, "parameter_optimisation_18sim_windcooling_windheating_5params_BFGS_extracted.jld2")
 
 file = jldopen(DATA_PATH, "r")
 
@@ -24,6 +31,26 @@ train_parameters = file["training_info/parameters"]
 
 𝒟train = WindMixing.data(train_files, scale_type=ZeroMeanUnitVarianceScaling, enforce_surface_fluxes=true)
 close(file)
+
+parameters_file = jldopen(PARAMETERS_PATH)
+mpp_parameters = parameters_file["parameters"]
+close(parameters_file)
+
+ν₀_initial = 1f-4
+ν₋_initial = 1f-1
+ΔRi_initial = 1f-1
+Riᶜ_initial = 0.25f0
+Pr_initial = 1f0
+
+mpp_scalings = 1 ./ [ν₀_initial, ν₋_initial, ΔRi_initial, Riᶜ_initial, Pr_initial]
+
+ν₀, ν₋, ΔRi, Riᶜ, Pr = mpp_parameters ./ mpp_scalings
+
+# ν₀ = train_parameters["ν₀"]
+# ν₋ = train_parameters["ν₋"]
+# ΔRi = train_parameters["ΔRi"]
+# Riᶜ = train_parameters["Riᶜ"]
+# Pr = 1f0
 
 N_inputs = 96
 hidden_units = 400
@@ -35,49 +62,26 @@ uw_NN = re(zeros(Float32, length(weights)))
 vw_NN = re(zeros(Float32, length(weights)))
 wT_NN = re(zeros(Float32, length(weights)))
 
-# to_run = ["-1e-3",                   
-#           "-9e-4",                   
-#           "-8e-4",                   
-#           "-7e-4",                   
-#           "-6e-4",                   
-#           "-5e-4",                   
-#           "-4e-4",                   
-#           "-3e-4",                   
-#           "-2e-4",                           
-#           "wind_-5e-4_cooling_4e-8", 
-#           "wind_-1e-3_cooling_4e-8", 
-#           "wind_-2e-4_cooling_1e-8", 
-#           "wind_-1e-3_cooling_2e-8", 
-#           "wind_-5e-4_cooling_1e-8", 
-#           "wind_-2e-4_cooling_5e-8", 
-#           "wind_-5e-4_cooling_3e-8", 
-#           "wind_-2e-4_cooling_3e-8", 
-#           "wind_-1e-3_cooling_3e-8", 
-#           "wind_-1e-3_heating_-4e-8",
-#           "wind_-1e-3_heating_-1e-8",
-#           "wind_-1e-3_heating_-3e-8",
-#           "wind_-5e-4_heating_-5e-8",
-#           "wind_-5e-4_heating_-3e-8",
-#           "wind_-5e-4_heating_-1e-8",
-#           "wind_-2e-4_heating_-5e-8",
-#           "wind_-2e-4_heating_-3e-8",
-#           "wind_-2e-4_heating_-1e-8",
-#           "cooling_6e-8",            
-#           "cooling_5e-8",            
-#           "cooling_4e-8",            
-#           "cooling_3e-8",            
-#           "cooling_2e-8",            
-#           "heating_-3e-8",
-#           ]
-
-to_run = [
-          "cooling_6e-8",            
-          "cooling_5e-8",            
-          "cooling_4e-8",            
-          "cooling_3e-8",            
-          "cooling_2e-8",            
-          "heating_-3e-8",
-]
+to_run = [                        
+          "wind_-5e-4_cooling_4e-8", 
+          "wind_-1e-3_cooling_4e-8", 
+          "wind_-2e-4_cooling_1e-8", 
+          "wind_-1e-3_cooling_2e-8", 
+          "wind_-5e-4_cooling_1e-8", 
+          "wind_-2e-4_cooling_5e-8", 
+          "wind_-5e-4_cooling_3e-8", 
+          "wind_-2e-4_cooling_3e-8", 
+          "wind_-1e-3_cooling_3e-8", 
+          "wind_-1e-3_heating_-4e-8",
+          "wind_-1e-3_heating_-1e-8",
+          "wind_-1e-3_heating_-3e-8",
+          "wind_-5e-4_heating_-5e-8",
+          "wind_-5e-4_heating_-3e-8",
+          "wind_-5e-4_heating_-1e-8",
+          "wind_-2e-4_heating_-5e-8",
+          "wind_-2e-4_heating_-3e-8",
+          "wind_-2e-4_heating_-1e-8",
+          ]
 
 for test_file in to_run
     @info "running $test_file"
@@ -86,8 +90,8 @@ for test_file in to_run
     trange = 1:1:1153
     plot_data = NDE_profile_mutating(uw_NN, vw_NN, wT_NN, 𝒟test, 𝒟train, trange,
                             modified_pacanowski_philander=train_parameters["modified_pacanowski_philander"], 
-                            ν₀=train_parameters["ν₀"], ν₋=train_parameters["ν₋"], ΔRi=train_parameters["ΔRi"], 
-                            Riᶜ=train_parameters["Riᶜ"], convective_adjustment=train_parameters["convective_adjustment"],
+                            ν₀=ν₀, ν₋=ν₋, ΔRi=ΔRi, Riᶜ=Riᶜ, Pr=Pr,
+                            convective_adjustment=train_parameters["convective_adjustment"],
                             smooth_NN=train_parameters["smooth_NN"], smooth_Ri=train_parameters["smooth_Ri"],
                             zero_weights=train_parameters["zero_weights"],
                             gradient_scaling=train_parameters["gradient_scaling"])
