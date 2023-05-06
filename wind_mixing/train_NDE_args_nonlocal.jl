d470ae803887b69c9d9e60f1fb1bee5f20364842
Xin Kai Lee <xin.lee18@imperial.ac.uk>
2023-05-06 19:06:51 -0400

    train NDE longer BFGS

--- wind_mixing/train_NDE_args_nonlocal.jl
+++ wind_mixing/train_NDE_args_nonlocal.jl
@@ -24,7 +24,7 @@ T_fraction = parse(Float32, T_fraction_str)
 NN_type = "leakyrelu"
 rate_str = "2e-4"
 rate = parse(Float64, rate_str)
-params_type = "18simLBFGST0.8nogradnonlocal"
+params_type = "18simBFGST0.8nogradnonlocal"
 
 n_layers = 1
 
@@ -76,7 +76,8 @@ VIDEO_PATH = joinpath(PATH, "Output")
 
 EXTRACTED_OUTPUT_PATH = joinpath(PATH, "extracted_training_output")
 
-FILE_NAME = "NDE_$(N_sims)sim_windcooling_windheating_$(params_type)_divide1f5_gradient_smallNN_$(NN_type)_layers_$(n_layers)_rate_2e-4_T$(T_fraction_str)_$(rate_str)_3"
+FILE_NAME = "NDE_$(N_sims)sim_windcooling_windheating_$(params_type)_divide1f5_gradient_smallNN_$(NN_type)_layers_$(n_layers)_rate_2e-4_T$(T_fraction_str)_$(rate_str)_300iter"
+
 # FILE_NAME = "test_$(params_type)_$(T_fraction)_$(NN_type)"
 FILE_PATH = joinpath(OUTPUT_PATH, "$(FILE_NAME).jld2")
 @assert !isfile(FILE_PATH)
@@ -159,57 +160,57 @@ end
 # vw_NN = vw_file["neural_network"]
 # wT_NN = wT_file["neural_network"]
 
-FILE_PATH_NN = joinpath(PATH, "extracted_training_output", 
-                        "NDE_$(N_sims)sim_windcooling_windheating_$(params_type)_divide1f5_gradient_smallNN_$(NN_type)_layers_$(n_layers)_rate_2e-4_T$(T_fraction_str)_2e-4_extracted.jld2")
-@assert isfile(FILE_PATH_NN)
+# FILE_PATH_NN = joinpath(PATH, "extracted_training_output", 
+#                         "NDE_$(N_sims)sim_windcooling_windheating_$(params_type)_divide1f5_gradient_smallNN_$(NN_type)_layers_$(n_layers)_rate_2e-4_T$(T_fraction_str)_2e-4_extracted.jld2")
+# @assert isfile(FILE_PATH_NN)
 
-file = jldopen(FILE_PATH_NN, "r")
+# file = jldopen(FILE_PATH_NN, "r")
 
-uw_NN = file["neural_network/uw"]
-vw_NN = file["neural_network/vw"]
-wT_NN = file["neural_network/wT"]
+# uw_NN = file["neural_network/uw"]
+# vw_NN = file["neural_network/vw"]
+# wT_NN = file["neural_network/wT"]
+# # close(file)
+
+# # train_parameters = file["training_info/parameters"]
+
+# # ν₀ = train_parameters["ν₀"]
+# # ν₋ = train_parameters["ν₋"]
+# # ΔRi = train_parameters["ΔRi"]
+# # Riᶜ = train_parameters["Riᶜ"]
+# # Pr = train_parameters["Pr"]
+
+# train_optimizers = [[ADAM(rate)]]
+# train_optimizers[1][1].beta = file["optimizer/β"]
+# train_optimizers[1][1].state = file["optimizer/state"]
+
 # close(file)
 
-# train_parameters = file["training_info/parameters"]
 
-# ν₀ = train_parameters["ν₀"]
-# ν₋ = train_parameters["ν₋"]
-# ΔRi = train_parameters["ΔRi"]
-# Riᶜ = train_parameters["Riᶜ"]
-# Pr = train_parameters["Pr"]
+N_inputs = 96
+hidden_units = 400
+N_outputs = 31
 
-train_optimizers = [[ADAM(rate)]]
-train_optimizers[1][1].beta = file["optimizer/β"]
-train_optimizers[1][1].state = file["optimizer/state"]
+if NN_type == "mish"
+  activation = mish
+elseif NN_type == "swish"
+  activation = swish
+elseif NN_type == "leakyrelu"
+  activation = leakyrelu
+elseif NN_type == "relu"
+  activation = relu
+else
+  activation = tanh
+end
 
-close(file)
+if n_layers == 1
+     weights, re = Flux.destructure(Chain(Dense(N_inputs, hidden_units, activation), Dense(hidden_units, N_outputs)))
+elseif n_layers == 2
+     weights, re = Flux.destructure(Chain(Dense(N_inputs, hidden_units, activation), Dense(hidden_units, hidden_units, activation), Dense(hidden_units, N_outputs)))
+end
 
-
-# N_inputs = 96
-# hidden_units = 400
-# N_outputs = 31
-
-# if NN_type == "mish"
-#   activation = mish
-# elseif NN_type == "swish"
-#   activation = swish
-# elseif NN_type == "leakyrelu"
-#   activation = leakyrelu
-# elseif NN_type == "relu"
-#   activation = relu
-# else
-#   activation = tanh
-# end
-
-# if n_layers == 1
-#      weights, re = Flux.destructure(Chain(Dense(N_inputs, hidden_units, activation), Dense(hidden_units, N_outputs)))
-# elseif n_layers == 2
-#      weights, re = Flux.destructure(Chain(Dense(N_inputs, hidden_units, activation), Dense(hidden_units, hidden_units, activation), Dense(hidden_units, N_outputs)))
-# end
-
-# uw_NN = re(weights ./ 1f5)
-# vw_NN = re(weights ./ 1f5)
-# wT_NN = re(weights ./ 1f5)
+uw_NN = re(weights ./ 1f5)
+vw_NN = re(weights ./ 1f5)
+wT_NN = re(weights ./ 1f5)
 
 # gradient_scaling = 5f-3
 diurnal = occursin("diurnal", train_files[1])
@@ -238,7 +239,7 @@ train_parameters = Dict(
 train_epochs = [1]
 train_tranges = [1:15:1153]
 train_iterations = [150]
-# train_optimizers = [[ADAM(rate)]]
+train_optimizers = [[ADAM(rate)]]
 
 # train_epochs = [1]
 # train_tranges = [1:20:200]
@@ -364,15 +365,15 @@ test_files = [
   "wind_-5e-4_diurnal_2e-8",    
   "wind_-5e-4_diurnal_3.5e-8",
   
-  # "cooling_5e-8_new",            
-  # "cooling_4.5e-8_new",          
-  # "cooling_4e-8_new",            
-  # "cooling_3.5e-8_new",         
-  # "cooling_3e-8_new",            
-  # "cooling_2.5e-8_new",          
-  # "cooling_2e-8_new",            
-  # "cooling_1.5e-8_new",          
-  # "cooling_1e-8_new",   
+  "cooling_5e-8_new",            
+  "cooling_4.5e-8_new",          
+  "cooling_4e-8_new",            
+  "cooling_3.5e-8_new",         
+  "cooling_3e-8_new",            
+  "cooling_2.5e-8_new",          
+  "cooling_2e-8_new",            
+  "cooling_1.5e-8_new",          
+  "cooling_1e-8_new",   
 
   "wind_-5e-4_new",              
   "wind_-4.5e-4_new",            
